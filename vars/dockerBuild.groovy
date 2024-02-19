e39b2e0a2b00d66dd3b0620115b892a197a6853d
ersinsari13 <sariiersinn13@gmail.com>
2024-02-19 11:42:37 +0300

    added new file

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -5,11 +5,3 @@ def call(String APP_IMAGE_REGISTRY, String APP_IMAGE_REPOSITORY) {
         sh "docker build -t ${APP_IMAGE_REGISTRY}/${APP_IMAGE_REPOSITORY}:$(BUILD_NUMBER) ."
     }
 }
-// vars/dockerPush.groovy
-#!/usr/bin/env groovy
-def call(String APP_IMAGE_REGISTRY, String APP_IMAGE_REPOSITORY) {
-    dir("${WORKSPACE}") {
-        sh "echo $DOCKERHUB_CRED_PSW | docker login -u $DOCKERHUB_CRED_USR --password-stdin"
-        sh "docker push ${APP_IMAGE_REGISTRY}/${APP_IMAGE_REPOSITORY}:$(BUILD_NUMBER)"
-    }
-}
---
+++ vars/dockerPush.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+def call(String APP_IMAGE_REGISTRY, String APP_IMAGE_REPOSITORY) {
+
+    dir("${WORKSPACE}") {
+        sh "echo $DOCKERHUB_CRED_PSW | docker login -u $DOCKERHUB_CRED_USR --password-stdin"
+        sh "docker push ${APP_IMAGE_REGISTRY}/${APP_IMAGE_REPOSITORY}:$(BUILD_NUMBER)"
+    }
+
+}
