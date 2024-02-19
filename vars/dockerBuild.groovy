db5661e754e46ea36a9bca5c616c2d92e2fd7213
ersinsari13 <sariiersinn13@gmail.com>
2024-02-19 13:35:43 +0300

    update

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
-// vars/dockerBuild.groovy
 #!/usr/bin/env groovy
+
 def call(String APP_IMAGE_REGISTRY, String APP_IMAGE_REPOSITORY) {
     dir("${WORKSPACE}") {
         sh "docker build -t ${APP_IMAGE_REGISTRY}/${APP_IMAGE_REPOSITORY}:$(BUILD_NUMBER) ."
